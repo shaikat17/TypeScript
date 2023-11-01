d1cd13d58fd5b4beccd96b76bd16b3782d33f7a8
shaikat17 <shaikatpal56@gmail.com>
2023-11-02 00:55:50 +0600

    Object, Optional Chaining, Literal Types.

---
+++ lesson 1/src/objectOptionalLiteral.ts
@@ -0,0 +1,14 @@
+// Reference Type --> Object
+
+const user:{
+    company: 'Programming Hero';    //type --> literal types
+    firstName: string;
+    middleName?: string;    //optional chaining
+    lastName: string;
+    isMarried: boolean;
+} = {
+    company: 'Programming Hero',
+    firstName: 'Shaikat',
+    lastName: 'Paul',
+    isMarried: true,
+}
