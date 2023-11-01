e59178ced52e5c79895d2651735aea43dacf4331
shaikat17 <shaikatpal56@gmail.com>
2023-11-02 00:14:12 +0600

    Basic Data Types.

---
+++ lesson 1/src/BasicDataTypes.ts
@@ -0,0 +1,30 @@
+// Basic Data Types
+
+// String
+let firstName: string = "Shaikat";
+
+// number
+let roll: number = 123;
+
+// boolean
+let isAdmin: boolean = true;
+
+// undefined
+let x:undefined = undefined
+
+// null
+let y:null = null;
+
+
+
+
+
+let d;      // refers to any type data; not recommended.
+
+
+// Array
+let friends: string[] = ['Rashel', 'Anik']
+let numbers: number[] = [10, 22, 25]
+
+// tuple
+let coordinate: [number, number] = [1,9]
