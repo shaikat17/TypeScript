7549ec733bcc85001c72bfbfb9639697cd46166f
shaikat17 <shaikatpal56@gmail.com>
2023-11-06 00:14:43 +0600

    Constraints

--- lesson_1/src/constraints.ts
+++ lesson_1/src/constraints.ts
@@ -1,7 +1,23 @@
 {
     // constraint
 
-    
+    const addCourseToStudent = <T extends {id: number; name: string; email: string;}>(student: T) => {
+        const course = "Complete JavaScript";
+
+        return {
+            ...student,
+            course
+        }
+    }
+
+    const student1 = addCourseToStudent({id: 22, name: "Shaikat", email: "shaikatpal56@gmail.com", watch: "apple"})
+
+    // const studentEx = addCourseToStudent({something: nai})
+
+    const student2 = addCourseToStudent({id: 33, name: "Anik", email: "anik@gmail.com", watch: "apple", developer: true})
+
+    console.log(student1)
+    console.log(student2)
 
     // 
 }
